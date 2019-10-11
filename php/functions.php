ac733278c7c66a7db26a3497417880a289917102
半桶水 <shenzhe163@gmail.com>
2019-10-11 17:00:46 +0800

    Swoole版本优化

--- php/functions.php
+++ php/functions.php
@@ -1,10 +1,10 @@
 <?php
-function text2sendjson( $text )
+function text2sendjson($text)
 {
-    $common = [ 'app_id' => XF_APPID ];
+    $common = ['app_id' => XF_APPID];
     $business = [];
-    $business['aue'] = "raw" ;
-    $business['auf'] = "audio/L16;rate=16000" ;
+    $business['aue'] = "raw";
+    $business['auf'] = "audio/L16;rate=16000";
     $business['vcn'] = "xiaoyuan";
     $business['tte'] = "UTF8";
 
@@ -12,28 +12,29 @@ function text2sendjson( $text )
     $data['status'] = 2;
 
 
-    $send_data = ['common'=>$common,'business'=>$business,'data'=>$data];return  json_encode( $send_data );
+    $send_data = ['common' => $common, 'business' => $business, 'data' => $data];
+    return  json_encode($send_data);
 }
 
-function get_xf_wss_info( $url = '/v2/tts' , $host = 'tts-api.xfyun.cn' )
+function get_xf_wss_info($url = '/v2/tts', $host = 'tts-api.xfyun.cn', $port = 80)
 {
     $date = gmdate('D, d M Y H:i:s') . ' GMT';
-    
+
     $request_line = "GET $url HTTP/1.1";
-    $signature_origin = "host: $host\ndate: $date\n".$request_line;
+    $signature_origin = "host: $host\ndate: $date\n" . $request_line;
     $signature_sha = hash_hmac('sha256', $signature_origin, XF_SKEY, true);
-    
-    $signature = base64_encode( $signature_sha );
-    
-    
-    $authorization_origin = 'api_key="'.XF_AKEY.'",algorithm="hmac-sha256",headers="host date request-line",signature="'.$signature.'"';
+
+    $signature = base64_encode($signature_sha);
 
 
-    $authorization = base64_encode( $authorization_origin );   
-    
-    $path = $url . '?authorization='.urlencode($authorization).'&host='.urlencode($host).'&date='.urlencode($date);
-    
-    $final_url = 'wss://'.$host.$path;
+    $authorization_origin = 'api_key="' . XF_AKEY . '",algorithm="hmac-sha256",headers="host date request-line",signature="' . $signature . '"';
 
-    return ['url'=>$final_url,'path'=>$path,'host'=>$host];
+
+    $authorization = base64_encode($authorization_origin);
+
+    $path = $url . '?authorization=' . urlencode($authorization) . '&host=' . urlencode($host) . '&date=' . urlencode($date);
+
+    $final_url = 'ws://' . $host . ':' . $port . $path;
+
+    return ['url' => $final_url, 'path' => $path, 'host' => $host, 'port' => $port];
 }
