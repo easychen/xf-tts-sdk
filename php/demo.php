e337982c744d1b4713245cd4143eca20e9951ba2
EasyChen <easychen@qq.com>
2019-10-12 09:54:07 +0800

    更新swoole版本

--- php/demo.php
+++ php/demo.php
@@ -1,35 +1,25 @@
 <?php
-define( "XF_APPID" , "5d948d69"  );
-define( "XF_SKEY" , "0c2ef87927af42270793ea3ad3c3d97e"  );
-define( "XF_AKEY" , "29bf437e6b9dcb28b0fad652596e6fa7"  );
 
-require_once 'functions.php';
-require_once 'websocket_client.php';
+use Swlib\SaberGM;
+
+Swoole\Runtime::enableCoroutine();
+require __DIR__ . '/vendor/autoload.php';
+
+define("XF_APPID", "5d948d69");
+define("XF_SKEY", "0c2ef87927af42270793ea3ad3c3d97e");
+define("XF_AKEY", "29bf437e6b9dcb28b0fad652596e6fa7");
 
 $info = get_xf_wss_info();
-
-$client = new WebSocketClient( $info['host'] , 80 , $info['path'
-] );
-
-$client->connect();
-
-$client->send( text2sendjson("测试音频") );
-
-$file = "append.pcm";
-
-sleep(5);
-
-$data = $client->recv();
-
-if( strlen( $data ) > 1000 )
-{
-    // websocket_client 或者 swoole 实现有问题，返回数据多了几个字节
-    // 导致 decode 不能成功
-    $data_array = json_decode( $data );
-    print_r( $data_array );
-    //file_put_contents( $file , $data );
-}
-else
-{
-    echo $data;
-}
+go(function () use ($info) {
+    $websocket = SaberGM::websocket($info['url']);
+    $websocket->push(text2sendjson("测试音频"));
+    $data = $websocket->recv();
+    if (strlen($data) > 1000) {
+        $data_array = json_decode($data);
+        print_r($data_array);
+        //可以写入文件
+        //file_put_contents( $file , $data );
+    } else {
+        echo $data;
+    }
+});
